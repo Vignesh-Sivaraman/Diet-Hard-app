ee1b2f27d79d3b826aed3227bb5f8fb253dc25ec
Sirius-18 <97686497+Vignesh-Sivaraman@users.noreply.github.com>
2022-12-04 20:48:11 +0530

    updated for vercel deployment

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 export const env = {
-  api: "https://diet-hard-server.herokuapp.com",
+  api: "https://diet-hard-server.vercel.app",
   id: "57076289",
   key: "9627c97e5fadd44ab800f63340048e9d",
 };
 
 // "http://localhost:3005"
-// "https://diet-hard-server.herokuapp.com"
+// "https://diet-hard-server.vercel.app"
